35a494835ae857072008ef23e610c31e4778a792
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2024-05-18 01:45:53 +0700

    Feat (backend) : create model user answer

---
+++ app/Models/UserAnswers.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property integer $quiz_attempt_id
+ * @property integer $question_id
+ * @property string $chosen_answer
+ * @property string $is_correct
+ * @property string $created_at
+ * @property string $updated_at
+ * @property QuizAttempt $quizAttempt
+ * @property Question $question
+ */
+class UserAnswers extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = ['quiz_attempt_id', 'question_id', 'chosen_answer', 'is_correct', 'created_at', 'updated_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function quizAttempt()
+    {
+        return $this->belongsTo('App\Models\QuizAttempt');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function question()
+    {
+        return $this->belongsTo('App\Models\Question');
+    }
+}
