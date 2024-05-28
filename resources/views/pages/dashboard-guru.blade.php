71f630eb9de44b172acaa891df239c252ffa5d43
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2024-05-28 16:09:39 +0700

    Fix (frontend) : perbaikan dashboard guru

--- resources/views/pages/dashboard-guru.blade.php
+++ resources/views/pages/dashboard-guru.blade.php
@@ -18,98 +18,34 @@
 
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-12 col-sm-12">
-                    <div class="section-header">
-                        <div class="d-flex flex-column">
-                            <h1>HALO !!!</h1>
-                            <p>Yuk kita belajar bersama</p>
-                        </div>
-                    </div>
-                </div>
-                {{-- <div class="col-lg-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Calendar</h4>
-                        </div>
-                        <div class="card-body">
-                            <div class="fc-overflow">
-                                <div id="myEvent"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div> --}}
-            </div>
-            <div class="section-header flex-column align-items-start ">
-                <div class="d-flex flex-column">
-                    <h1>Announcement</h1>
-                </div>
-                <div class="col-12">
-                    <div class="card  py-1 mt-4 " style="background-color: #D4DEFF; border-radius:20px">
 
-                        <div class="card-wrap">
 
-                            <div class="card-body text-dark d-flex justify-content-between align-items-center ">
-
-                                <div>
-                                    <h6 class="text-capitalize">
-                                        Hello {{ Session('user')['nama'] }}
-                                    </h6>
-                                    <h6>
-                                        {{-- {{ $newest_notifikasi->judul }} --}}
-                                    </h6>
-                                </div>
-                                <div>
-                                    <a href="/student/materi" class="btn btn-primary btn-lg rounded-pill">
-                                        Open
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
             <div class="row">
-                <div class="col-lg-9 col-md-12 col-12 col-sm-12">
+                <div class="col-9">
                     <div class="section-header flex-column align-items-start ">
                         <div class="d-flex flex-column">
-                            <h1>Assignments</h1>
+                            <h1>My Students</h1>
                         </div>
-                        <div class="col-12">
-                            <div class="card bg-light secondary pt-3 px-4 mt-4 " style=" border-radius:20px">
+                        <div class="d-flex justify-content-center align-items-center w-100 mt-5">
 
-                                <div class="card-wrap">
-
-                                    <div class="card-body text-dark d-flex justify-content-between align-items-center ">
-                                        <div>
-                                            <h6 class="">
-                                                Chemistry
-                                            </h6>
-                                            <p>
-                                                Chapter 5 </p>
-                                        </div>
-
-                                        <div>
-                                            <h6 class="">
-                                                Daily task
-                                            </h6>
-                                            <p>
-                                                Page 10 </p>
-                                        </div>
-                                        <div>
-                                            <h6>
-                                                11.00 AM
-                                            </h6>
-                                        </div>
-                                        <div class="text-warning">
-                                            <h6>
-                                                Pending
-                                            </h6>
+                            @foreach ($listMurid as $list)
+                                {{-- <div class="col-3"> --}}
+                                <article class="article article-style-b mr-4" style="width: 15vw">
+                                    <div class="article-header">
+                                        <div class="article-image" data-background="{{ asset('img/news/img15.jpg') }}">
                                         </div>
                                     </div>
-                                </div>
-                            </div>
+                                    <div class="article-details">
+                                        <div class="article-title">
+                                            <h2 class="text-capitalize"><a href="#">{{ $list->nama_lengkap }}</a>
+                                            </h2>
+                                        </div>
+
+                                    </div>
+                                </article>
+                                {{-- </div> --}}
+                            @endforeach
                         </div>
                     </div>
                 </div>
@@ -120,42 +56,19 @@
                         </div>
                         <div class="card-body">
                             <ul class="list-unstyled list-unstyled-border">
-                                <li class="media">
-                                    <img class="rounded-circle mr-3" width="50"
-                                        src="{{ asset('img/avatar/avatar-1.png') }}" alt="avatar">
-                                    <div class="media-body">
+                                @foreach ($list_leaderboard as $list_ranking)
+                                    <li class="media">
+                                        <img class="rounded-circle mr-3" width="50"
+                                            src="{{ asset('img/avatar/avatar-1.png') }}" alt="avatar">
+                                        <div class="media-body">
 
-                                        <div class="media-title">Mingyu</div>
-                                        <span class="text-small text-muted">Score 60</span>
-                                    </div>
-                                </li>
-                                <li class="media">
-                                    <img class="rounded-circle mr-3" width="50"
-                                        src="{{ asset('img/avatar/avatar-1.png') }}" alt="avatar">
-                                    <div class="media-body">
+                                            <div class="media-title">{{ $list_ranking->nama_lengkap }}</div>
+                                            <span class="text-small text-muted">Score {{ $list_ranking->score }}</span>
+                                        </div>
+                                    </li>
+                                @endforeach
 
-                                        <div class="media-title">Mingyu</div>
-                                        <span class="text-small text-muted">Score 60</span>
-                                    </div>
-                                </li>
-                                <li class="media">
-                                    <img class="rounded-circle mr-3" width="50"
-                                        src="{{ asset('img/avatar/avatar-1.png') }}" alt="avatar">
-                                    <div class="media-body">
 
-                                        <div class="media-title">Mingyu</div>
-                                        <span class="text-small text-muted">Score 60</span>
-                                    </div>
-                                </li>
-                                <li class="media">
-                                    <img class="rounded-circle mr-3" width="50"
-                                        src="{{ asset('img/avatar/avatar-1.png') }}" alt="avatar">
-                                    <div class="media-body">
-
-                                        <div class="media-title">Mingyu</div>
-                                        <span class="text-small text-muted">Score 60</span>
-                                    </div>
-                                </li>
                             </ul>
                             <div class="pt-1 pb-1 text-center">
                                 <a href="#" class="btn btn-primary btn-lg btn-round">
