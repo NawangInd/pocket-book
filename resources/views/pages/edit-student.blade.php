eadebde7689d07a65929387c610ec90443c75a56
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2024-05-30 02:48:52 +0700

    Feat (frontend) : halaman edit murid

---
+++ resources/views/pages/edit-student.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.app')
+
+@section('title', 'Edit Materi')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/summernote/dist/summernote-bs4.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/codemirror/lib/codemirror.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/codemirror/theme/duotone-dark.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Edit Student</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item"><a href="#">Materi</a></div>
+                    <div class="breadcrumb-item">Edit Student</div>
+                </div>
+            </div>
+
+            <div class="section-body">
+
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Form Edit Student</h4>
+                            </div>
+                            <form class="form" action="/teacher/manage-student/{{ Request::segment(3) }}" method="post"
+                                enctype="multipart/form-data">
+                                @csrf
+                                @method('PUT')
+                                <div class="card-body">
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nama
+                                            Lengkap</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="nama_lengkap"
+                                                value="{{ $murid->nama_lengkap }}">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Email</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="email" class="form-control" name="email"
+                                                value="{{ $murid->email }}">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-4">
+                                        <label
+                                            class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Password</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="password"
+                                                value="{{ $murid->password }}">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nomor
+                                            Induk</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="number" min="0" class="form-control" name="nomor_induk"
+                                                value="{{ $murid->nomor_induk }}">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Alamat</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <textarea class="form-control" name="alamat">
+                                                {{ $murid->alamat }}
+                                            </textarea>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <button class="btn btn-primary" type="submit">Publish</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+    <script src="{{ asset('library/summernote/dist/summernote-bs4.js') }}"></script>
+    <script src="{{ asset('library/codemirror/lib/codemirror.js') }}"></script>
+    <script src="{{ asset('library/codemirror/mode/javascript/javascript.js') }}"></script>
+    <script src="{{ asset('library/selectric/public/jquery.selectric.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+@endpush
