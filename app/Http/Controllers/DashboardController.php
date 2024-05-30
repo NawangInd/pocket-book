a9b6c3bdca80565f0ce15724caec4074e804109a
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2024-05-31 01:50:06 +0700

    Fix (backend) : perbaikan dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -40,10 +40,16 @@ class DashboardController extends Controller
         $newest_notifikasi = Notifikasi::where('role', '=', 'Murid')->orderBy('id', 'desc')->first();
         $get_new_materi = Materi::latest()->first();
         $get_new_quiz = Quizzes::where("materi_id", "=", $get_new_materi->id)->first();
-        $list_leaderboard = QuizAttempts::join('user', 'user.id',  '=', 'quiz_attempts.user_id')->where("quizzes_id", "=", $get_new_quiz->id)->select('quiz_attempts.*', 'user.nama_lengkap')->get();
+
+        if ($get_new_quiz !== null) {
+            $list_leaderboard = QuizAttempts::join('user', 'user.id',  '=', 'quiz_attempts.user_id')->where("quizzes_id", "=", $get_new_quiz->id)->select('quiz_attempts.*', 'user.nama_lengkap')->get();
+        } else {
+            $get_newest_quiz = Quizzes::orderBy('id', 'desc')->first();
+            $list_leaderboard = QuizAttempts::join('user', 'user.id',  '=', 'quiz_attempts.user_id')->where("quizzes_id", "=", $get_newest_quiz->id)->select('quiz_attempts.*', 'user.nama_lengkap')->get();
+        }
         $listMurid = User::where('role', '=', 'Murid')->limit('4')->get();
 
-        // dd($listMurid);
+        // dd($get_new_materi->id);
 
 
 
