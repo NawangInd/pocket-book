edf8d4942a146efa989564a2f26924ea8685bf92
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2024-06-03 01:56:00 +0700

    Fix (backend) : perbaikan quiz controller

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Session;
 use App\models\Admin;
 use App\Models\Assignment;
+use App\Models\AssignmentSubmission;
 use App\Models\Materi;
 use App\Models\Notifikasi;
 use App\Models\Quizzes;
@@ -26,17 +27,18 @@ class AssignmentController extends Controller
         return view('pages.assignment-guru', compact('data'));
     }
 
-    public function indexMateriMurid()
+    public function indexAssignmentMurid()
     {
-        $data = Materi::join('user', 'user.id', '=', 'materi.user_id')
-            ->select('materi.*', 'user.nama_lengkap')
-            ->orderBy('id', 'desc')
+        $data = Assignment::join('materi', 'materi.id', '=', 'assignment.materi_id')
+            ->select('assignment.*', 'materi.judul as judul_materi')
+            // ->orderBy('id', 'desc')
             ->get();
 
 
 
+
         // dd($data);
-        return view('pages.materi', compact('data'));
+        return view('pages.assignment', compact('data'));
     }
 
 
@@ -105,15 +107,54 @@ class AssignmentController extends Controller
         // $data['karyawan'] = Pegawai::where([
         //     'id' => $request->segment(3)
         // ])->first();
-        $assignment = Assignment::where([
-            'id' => $request->segment(3)
-        ])->first();
-
-        // dd($assignment);
-        $materi = Materi::all();
 
 
-        return view('pages.edit-assignment', compact('assignment', 'materi'));
+        // dd($request->segment(4));
+
+        if (Session('user')['role'] == 'Guru') {
+            $assignment = Assignment::where([
+                'id' => $request->segment(3)
+            ])->first();
+
+            $materi = Materi::all();
+
+            return view('pages.edit-assignment', compact('assignment', 'materi'));
+        } else {
+            $assignment = Assignment::where([
+                'id' => $request->segment(4)
+            ])->with('materi')->first();
+
+            // dd($assignment->materi->judul);
+
+            return view('pages.detail-assignment', compact('assignment'));
+        }
+    }
+
+    public function viewSubmissions($id)
+    {
+        // dd($id);
+        $murid = User::where('role', '=', 'Murid')->get();
+        // dd($murid);
+        $data = [];
+
+        foreach ($murid as $list_murid) {
+            $cek = AssignmentSubmission::where('assignment_id', '=', $id)
+                ->where('user_id', '=', $list_murid->id)
+                ->first();
+
+            $data[] = [
+                'nama_lengkap' => $list_murid->nama_lengkap,
+                'status' => $cek ? $cek->status : "Belum Mengumpulkan",
+                'tgl_submit' => $cek ? Carbon::parse($cek->created_at) : NULL,
+                'file' => $cek ? $cek->file : NULL,
+            ];
+
+            // dd($cek);
+
+            # code...
+        }
+        // dd($data);
+        return view('pages.view-submissions', compact('data'));
     }
 
     public function update(Request $request)
@@ -174,14 +215,58 @@ class AssignmentController extends Controller
 
     public function destroy(Request $request, $id)
     {
-        $assignment = Assignment::findOrFail($id);
+        // $assignment = Assignment::findOrFail($id);
+        $getAssignmentSubmission = AssignmentSubmission::where('assignment_id', '=', $id)->get();
+        if ($getAssignmentSubmission) {
+            $deleteAssignmentSubmission = AssignmentSubmission::where('assignment_id', '=', $id)->delete();
 
-
-
-        if ($assignment->delete()) {
-            return redirect('/teacher/assignment');
+            if ($deleteAssignmentSubmission) {
+                Assignment::where('id', $id)->delete();
+                return redirect('/teacher/assignment');
+            }
         } else {
+            Assignment::where('id', $id)->delete();
             return redirect('/teacher/assignment');
         }
+
+
+        // if ($assignment->delete()) {
+        //     return redirect('/teacher/assignment');
+        // } else {
+        //     return redirect('/teacher/assignment');
+        // }
+    }
+
+    public function submitSubmission(Request $request, $id)
+    {
+        // dd($id);
+        if ($request->hasFile('file')) {
+            // dd($request->all());
+
+            $file = $request->file('file')->getClientOriginalName();
+            $request->file('file')->move('file_upload/submission/', $file);
+
+
+            // Ambil assignment berdasarkan ID
+            $assignment = Assignment::findOrFail($id);
+            // dd($assignment);
+
+            // Periksa apakah pengumpulan dilakukan sebelum atau setelah deadline
+            $currentDateTime = Carbon::now();
+            $endTime = Carbon::parse($assignment->end_date);
+            // dd($assignment->end_date);
+
+            $status = $currentDateTime->lessThanOrEqualTo($endTime) ? "Sudah Mengumpulkan" : "Terlambat";
+
+
+            $submission = new AssignmentSubmission;
+            $submission->user_id = Session('user')['id'];
+            $submission->assignment_id = $id;
+            $submission->file = $file;
+            $submission->status = $status;
+            $submission->save();
+            return redirect('/student/assignment/submission/' . $id);
+        }
+        return redirect('assignment/submission/submission/' . $id);
     }
 }
